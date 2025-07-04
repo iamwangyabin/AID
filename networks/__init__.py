e41e72a8dc0b738f159346b1ba20388b33ce3d8e
Wang Yabin <38123329+iamwangyabin@users.noreply.github.com>
2025-07-04 23:11:30 +0800

    add cnndet

--- networks/__init__.py
+++ networks/__init__.py
@@ -3,5 +3,7 @@ from .ojha_detector import OjhaModel
 from .npr_detector import NPRModel
 from .freqnet_detector import FreqNet
 from .gramnet_detector import GramNet
+from .cnndet import CNNDet
 
-__all__ = ['RINEModel', 'OjhaModel', 'NPRModel', 'GramNet', 'FreqNet']
+__all__ = ['RINEModel', 'OjhaModel', 'NPRModel', 
+           'GramNet', 'FreqNet', 'CNNDet']
