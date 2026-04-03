b0b1a7ae334a41691b92991381371b3308ecd6d8
Wang Yabin <38123329+iamwangyabin@users.noreply.github.com>
2026-04-03 12:02:33 +0800

    Add num_sanity_val_steps config and use in Trainer
    Expose PyTorch Lightning's num_sanity_val_steps in the training config and forward it to the Trainer. Added num_sanity_val_steps: 0 to cfgs/train/official/vjepa2_1_linear.yaml to disable sanity validation for that config, and updated train.py to pass conf.train.get('num_sanity_val_steps', 2) into L.Trainer (fallback default 2). This allows controlling startup sanity-check steps via config.

--- train.py
+++ train.py
@@ -83,10 +83,10 @@ if __name__ == '__main__':
     trainer = L.Trainer(logger=wandb_logger, max_epochs=conf.train.train_epochs, accelerator="gpu", devices=conf.train.gpu_ids,
                         callbacks=[checkpoint_callback],
                         check_val_every_n_epoch=conf.train.check_val_every_n_epoch,
+                        num_sanity_val_steps=conf.train.get('num_sanity_val_steps', 2),
                         precision=conf.train.get('precision', "16"))
 
     trainer.fit(model=model, train_dataloaders=train_loader, val_dataloaders=val_loader)
 
     trainer.save_checkpoint(os.path.join('logs', today_str, "last.ckpt"))
-
 
